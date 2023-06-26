b3bf4ba7fbd10643831899b3a73a9d528bdb2aec
unknown <masoodadnan782@gmail.com>
2023-06-26 18:00:28 +0500

    first commit

---
+++ app/src/main/java/com/example/modernfurniture/add_product_users.java
@@ -0,0 +1,14 @@
+package com.example.modernfurniture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_product_users extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product_users);
+    }
+}
